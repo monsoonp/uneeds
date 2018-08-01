3b8e9a09efe12baff78ea7fafdbe3c8d550e4099
user <user@LAPTOP-F7VMTQH3>
2018-08-01 15:02:34 +0900

    맛집 데이터

---
+++ uneeds/src/main/java/com/food/domain/Food_dataVo.java
@@ -0,0 +1,112 @@
+package com.food.domain;
+
+public class Food_dataVo {
+	private int fid, kid, lid;
+	private String fname, fdetail, faddr, fphone, fgmenu, fimg1, fimg2, fimg3;
+	
+	public Food_dataVo() {}
+
+	public Food_dataVo(int fid, int kid, int lid, String fname, String fdetail, String faddr, String fphone, String fgmenu,
+			String fimg1, String fimg2, String fimg3) {
+		super();
+		this.fid = fid;
+		this.kid = kid;
+		this.lid = lid;
+		this.fname = fname;
+		this.fdetail = fdetail;
+		this.faddr = faddr;
+		this.fphone = fphone;
+		this.fgmenu = fgmenu;
+		this.fimg1 = fimg1;
+		this.fimg2 = fimg2;
+		this.fimg3 = fimg3;
+	}
+
+	public int getLid() {
+		return lid;
+	}
+
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+
+	public int getFid() {
+		return fid;
+	}
+
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+
+	public int getKid() {
+		return kid;
+	}
+
+	public void setKid(int kid) {
+		this.kid = kid;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getFdetail() {
+		return fdetail;
+	}
+
+	public void setFdetail(String fdetail) {
+		this.fdetail = fdetail;
+	}
+
+	public String getFaddr() {
+		return faddr;
+	}
+
+	public void setFaddr(String faddr) {
+		this.faddr = faddr;
+	}
+
+	public String getFphone() {
+		return fphone;
+	}
+
+	public void setFphone(String fphone) {
+		this.fphone = fphone;
+	}
+
+	public String getFgmenu() {
+		return fgmenu;
+	}
+
+	public void setFgmenu(String fgmenu) {
+		this.fgmenu = fgmenu;
+	}
+
+	public String getFimg1() {
+		return fimg1;
+	}
+
+	public void setFimg1(String fimg1) {
+		this.fimg1 = fimg1;
+	}
+
+	public String getFimg2() {
+		return fimg2;
+	}
+
+	public void setFimg2(String fimg2) {
+		this.fimg2 = fimg2;
+	}
+
+	public String getFimg3() {
+		return fimg3;
+	}
+
+	public void setFimg3(String fimg3) {
+		this.fimg3 = fimg3;
+	}
+}
---
+++ uneeds/src/main/webapp/WEB-INF/views/food/main.jsp
@@ -0,0 +1,27 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="/resources/css/food_main.css">
+<title>UNEEDS FOOD</title>
+</head>
+<body>
+	<div id="main_logo">
+	<a href="main"><img alt="로고" src="/resources/image/logo_main.png"></a>
+	</div>
+	<div id="main_first_message">
+		<ul style="text-align: center;">
+			<li>UNEEDS<br>FOOD</li>
+		</ul>
+		<div id="main_search" style="position: relative; margin-top: 40px;">
+    		<input id='input' type="text">
+    		<span id=input_span>
+    		<button id="btn_input"><a href="search" style="color: #fff; text-decoration: none;">Search</a></button>
+    		</span>
+  		</div>
+	</div>
+</body>
+</html>
